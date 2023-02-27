c4e45de86c7cc64c65432737b756a14486a18aeb
Rayan-Aguiar <rayansiqueira@gmail.com>
2023-02-27 17:02:04 -0300

    fix bug api openWeather

--- js/script.js
+++ js/script.js
@@ -1,1 +1,38 @@
-// 68449285ed16d1db75c8594a5890271e
+//Variaveis e seleção de eventos
+const apiKey = "06b7b728df06c1e8e10d06469d7b163c";
+const apiCountryURL = "https://countryflagsapi.com/png/";
+
+const cityInput = document.querySelector("#city-input");
+const searchBtn = document.querySelector("#search");
+
+const cityElement = document.querySelector("#city")
+const tempElement = document.querySelector("#temperature span")
+const descElement = document.querySelector("#description")
+const weatherIconElement = document.querySelector("#weather-icon")
+const countryElement = document.querySelector("#country")
+const umidityElement = document.querySelector("#umidity span")
+const windElement = document.querySelector("#wind span")
+// Funções
+
+const getWeatherData = async(city) =>{
+    const apiWeatherURL = `https://api.openweathermap.org/data/2.5/weather?q=${city}&units=metric&appid=${apiKey}&lang=pt_br`
+
+    const res = await fetch(apiWeatherURL)
+    const data = await res.json()
+
+    console.log(data)
+}
+
+const showWeatherData = (city) =>{
+    getWeatherData(city);
+}
+
+// Eventos
+
+searchBtn.addEventListener("click", (e) => {
+    e.preventDefault();
+
+    const city = cityInput.value;
+
+    showWeatherData(city);
+})
